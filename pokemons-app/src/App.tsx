e26be6ab68230925694f82e6508784ee2f64b689
tbrault <heymarin@hotmail.fr>
2022-08-18 20:57:08 +0200

    add liste pokemons

--- pokemons-app/src/App.tsx
+++ pokemons-app/src/App.tsx
@@ -11,8 +11,28 @@ const App: FunctionComponent = () => {
 
   return (
     <div>
-      <h1>Pokedex</h1>
-      <p>Il y a {pokemons.length} pokémons dans le pokedex</p>
+      <h1 className="center">Pokedex</h1>
+      <div className="container">
+        <div className="row">
+          {pokemons.map(({ id, name, picture, created }) => (
+            <div className="col s6 m4" key={id}>
+              <div className="card horizontal">
+                <div className="card-image">
+                  <img src={picture} alt={name} />
+                </div>
+                <div className="card-stacked">
+                  <div className="card-content">
+                    <p>{name}</p>
+                    <p>
+                      <small>{created.toString()}</small>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
