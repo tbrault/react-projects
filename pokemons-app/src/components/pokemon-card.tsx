26798f248208a30e713f554ca94b14a8ea579987
tbrault <heymarin@hotmail.fr>
2022-08-19 11:18:04 +0200

    add proprietes calcules

--- pokemons-app/src/components/pokemon-card.tsx
+++ pokemons-app/src/components/pokemon-card.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent, useState } from "react";
 import Pokemon from "../models/pokemon";
 import "./pokemon-card.css";
+import formatDate from "../helpers/format-date";
+import formatType from "../helpers/format-type";
 
 type Props = {
   pokemon: Pokemon;
@@ -35,8 +37,13 @@ const PokemonCard: FunctionComponent<Props> = ({
           <div className="card-content">
             <p>{pokemon.name}</p>
             <p>
-              <small>{pokemon.created.toString()}</small>
+              <small>{formatDate(pokemon.created)}</small>
             </p>
+            {pokemon.types.map((type) => (
+              <span key={type} className={formatType(type)}>
+                {type}
+              </span>
+            ))}
           </div>
         </div>
       </div>
