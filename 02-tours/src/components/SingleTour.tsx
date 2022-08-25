1a5e7d5813d95b5bd4898fd62ea31eea0a3143e9
tbrault <heymarin@hotmail.fr>
2022-08-25 19:15:14 +0200

    display tours

--- 02-tours/src/components/SingleTour.tsx
+++ 02-tours/src/components/SingleTour.tsx
@@ -7,15 +7,27 @@ type Props = {
 
 const SingleTour: FunctionComponent<Props> = ({ tour }) => {
   const { name, info, image, price } = tour;
+  const [readMore, setReadMore] = useState<boolean>(false);
 
   return (
-    <div>
-      <h2>tour component</h2>
-      <p>{name}</p>
-      <p>{info}</p>
-      <p>{image}</p>
-      <p>{price}</p>
-    </div>
+    <>
+      <article className="single-tour">
+        <img src={image} alt={name} />
+        <footer>
+          <div className="tour-info">
+            <h4>{name}</h4>
+            <h4 className="tour-price">${price}</h4>
+          </div>
+          <p>
+            {readMore ? info : `${info.substring(0, 200)}...`}
+            <button onClick={() => setReadMore(!readMore)}>
+              {readMore ? "Show Less" : "Read More"}
+            </button>
+          </p>
+          <button className="delete-btn">not interested</button>
+        </footer>
+      </article>
+    </>
   );
 };
 
