13ba46f4aa87b2b55b772035f9968b0bf2766959
tbrault <heymarin@hotmail.fr>
2022-08-26 11:55:43 +0200

    refactoring  reviews app

--- 03-reviews/src/components/Review.tsx
+++ 03-reviews/src/components/Review.tsx
@@ -2,58 +2,38 @@ import React, { useState, FunctionComponent } from "react";
 import people from "../data/data";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
-type Person = {
-  id: number;
-  name: string;
-  job: string;
-  image: string;
-  text: string;
-};
-
 const Review: FunctionComponent = () => {
-  const [person, setPerson] = useState<Person>(people[0]);
-
-  const getPreviousPerson = () => {
-    if (person.id === 1) {
-      setPerson(people[people.length - 1]);
-    } else {
-      setPerson(people[person.id - 2]);
-    }
-  };
-
-  const getNextPerson = () => {
-    if (person.id === people.length) {
-      setPerson(people[0]);
-    } else {
-      setPerson(people[person.id]);
-    }
-  };
+  const [index, setIndex] = useState<number>(0);
+  const { name, image, job, text } = people[index % people.length];
 
   const getRandomIndex = () => {
     return Math.floor(Math.random() * people.length);
   };
 
   const getRandomPerson = () => {
-    const index = getRandomIndex();
-    setPerson(people[index]);
+    let randomIndex = getRandomIndex();
+    if (randomIndex === index) {
+      randomIndex++;
+    }
+    setIndex(randomIndex);
   };
 
   return (
     <article className="review">
       <div className="img-container">
-        <img src={person.image} alt={person.name} className="person-img " />
+        <img src={image} alt={name} className="person-img " />
         <span className="quote-icon">
           <FaQuoteRight />
         </span>
       </div>
-      <h4 className="author">{person.name}</h4>
-      <p className="job">{person.job}</p>
-      <p className="info">{person.text}</p>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
       <div>
-        <button className="prev-btn" onClick={getPreviousPerson}>
+        <button className="prev-btn" onClick={() => setIndex(index - 1)}>
           <FaChevronLeft />
         </button>
-        <button className="next-btn" onClick={getNextPerson}>
+        <button className="next-btn" onClick={() => setIndex(index + 1)}>
           <FaChevronRight />
         </button>
       </div>
