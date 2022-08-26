a5f657e0599d8884e3ee3eca91b30fb0f592a36d
tbrault <heymarin@hotmail.fr>
2022-08-26 11:11:34 +0200

    init all projects useeffect

---
+++ 04-accordion/src/App.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import data from "./data/data";
+import SingleQuestion from "./components/Question";
+function App() {
+  return <h2>accordion project setup</h2>;
+}
+
+export default App;
---
+++ 05-menu/src/App.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import Menu from "./components/Menu";
+import Categories from "./components/Categories";
+import items from "./data/data";
+
+function App() {
+  return <h2>menu project setup</h2>;
+}
+
+export default App;
