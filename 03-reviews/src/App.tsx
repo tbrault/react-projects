7ef22f6833cd99c677918e8462d3d62f19ffa4b1
tbrault <heymarin@hotmail.fr>
2022-08-26 10:20:02 +0200

    add display reviwe and prev next buttons

---
+++ 03-reviews/src/App.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from "react";
+import Review from "./components/Review";
+
+const App: FunctionComponent = () => {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
+};
+
+export default App;
---
+++ 03-reviews/src/components/Review.tsx
@@ -0,0 +1,56 @@
+import React, { useState, FunctionComponent } from "react";
+import people from "../data/data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+type Person = {
+  id: number;
+  name: string;
+  job: string;
+  image: string;
+  text: string;
+};
+
+const Review: FunctionComponent = () => {
+  const [person, setPerson] = useState<Person>(people[0]);
+
+  const getPreviousPerson = () => {
+    if (person.id === 1) {
+      setPerson(people[people.length - 1]);
+    } else {
+      setPerson(people[person.id - 2]);
+    }
+  };
+
+  const getNextPerson = () => {
+    if (person.id === people.length) {
+      setPerson(people[0]);
+    } else {
+      setPerson(people[person.id]);
+    }
+  };
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={person.image} alt={person.name} className="person-img " />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{person.name}</h4>
+      <p className="job">{person.job}</p>
+      <p className="info">{person.text}</p>
+      <div>
+        <button className="prev-btn" onClick={getPreviousPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={getNextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn">surprise me</button>
+    </article>
+  );
+};
+
+export default Review;
