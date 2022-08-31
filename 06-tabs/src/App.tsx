cc439bdee171a5c2459be370c2b2ae782443ba26
tbrault <heymarin@hotmail.fr>
2022-08-31 20:09:55 +0200

    setup tabs app

--- 06-tabs/src/App.tsx
+++ 06-tabs/src/App.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, { useState, useEffect, FunctionComponent } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
-function App() {
-  return <h2>tabs project setup</h2>
-}
+const url = "https://course-api.com/react-tabs-project";
 
-export default App
+const App: FunctionComponent = () => {
+  return <h2>tabs project setup</h2>;
+};
+
+export default App;
