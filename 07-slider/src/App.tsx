1d5dac9e7effb86f8786dc3dfa95a9963f14ec96
T0252166 <teddy.brault@thalesgroup.com>
2022-09-21 11:09:25 +0200

    setup app finished

--- 07-slider/src/App.tsx
+++ 07-slider/src/App.tsx
@@ -1,10 +1,22 @@
 import React, { useState, useEffect, FunctionComponent } from "react";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
-import data from "./data/data";
+import people from "./data/data";
 
 const App: FunctionComponent = () => {
-  const [reviews, setReviews] = useState(data);
+  const [index, setIndex] = useState<number>(0);
+
+  const getRandomIndex = () => {
+    return Math.floor(Math.random() * people.length);
+  };
+
+  const getRandomPerson = () => {
+    let randomIndex = getRandomIndex();
+    if (randomIndex === index) {
+      randomIndex++;
+    }
+    setIndex(randomIndex);
+  };
 
   return (
     <section className="section">
@@ -14,13 +26,19 @@ const App: FunctionComponent = () => {
         </h2>
       </div>
       <div className="section-center">
-        <article>
-          <img src="" alt="" className="person-img" />
-          <h4>test</h4>
-          <p className="title">title</p>
-          <p className="text">text</p>
-          <FaQuoteRight className="icon" />
-        </article>
+        {people.map((person) => {
+          const { name, image, title, quote, id } = person;
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+
         <button className="prev" type="button">
           <FiChevronLeft />
         </button>
