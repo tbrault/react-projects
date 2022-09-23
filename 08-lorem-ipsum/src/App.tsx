4d658a718f098203f16160c9b999a62e8bcd55c6
tbrault <heymarin@hotmail.fr>
2022-09-23 15:18:43 +0200

    setup display

--- 08-lorem-ipsum/src/App.tsx
+++ 08-lorem-ipsum/src/App.tsx
@@ -1,7 +1,20 @@
 import React, { useState } from "react";
 import data from "./data/data";
+
 function App() {
-  return <h2>lorem ipsum project setup</h2>;
+  return (
+    <section className="section-center">
+      <h3>TIRED OF BORING LOREM IPSUM?</h3>
+      <form className="lorem-form">
+        <label htmlFor="numberOfParagraphs">paragraphs: </label>
+        <input type="text" />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <p className="result"></p>
+    </section>
+  );
 }
 
 export default App;
