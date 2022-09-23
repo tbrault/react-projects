6eb91c8b1ba3a2b1895bd3b7a7f9bab2f1797f9a
tbrault <heymarin@hotmail.fr>
2022-09-23 21:16:33 +0200

    app 8 finished

--- 08-lorem-ipsum/src/App.tsx
+++ 08-lorem-ipsum/src/App.tsx
@@ -2,23 +2,39 @@ import React, { useState } from "react";
 import data from "./data/data";
 
 function App() {
-  const [numberOfParagraphs, setNumebrOfParagraphs] = useState<number>(2);
+  const [paragraphs, setParagraphs] = useState<number>(0);
+  const [paragraphsDisplayed, setParagraphsDisplayed] = useState<string[]>([]);
 
-  const paragraphs = data.slice(0, numberOfParagraphs);
+  const handleSubmit = (e: React.FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    let amount = paragraphs;
+    if (paragraphs < 1) {
+      amount = 1;
+    }
+    if (paragraphs > 8) {
+      amount = 8;
+    }
+    setParagraphsDisplayed(data.slice(0, amount));
+  };
 
   return (
-    <section className="section-center">
-      <h3>TIRED OF BORING LOREM IPSUM?</h3>
+    <section className="section-center" onSubmit={handleSubmit}>
+      <h3>tired of boring lorem ipsum?</h3>
       <form className="lorem-form">
         <label htmlFor="numberOfParagraphs">paragraphs: </label>
-        <input type="number" value={numberOfParagraphs} />
+        <input
+          type="number"
+          id="amount"
+          value={paragraphs}
+          onChange={(e) => setParagraphs(+e.target.value)}
+        />
         <button type="submit" className="btn">
           generate
         </button>
       </form>
       <article className="result">
-        {paragraphs.map((paragraph) => {
-          return <p>{paragraph}</p>;
+        {paragraphsDisplayed.map((paragraph, index) => {
+          return <p key={index}>{paragraph}</p>;
         })}
       </article>
     </section>
