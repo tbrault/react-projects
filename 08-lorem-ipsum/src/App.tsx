9e6ecd0457a3328219cd867c186686a64e45d34a
tbrault <heymarin@hotmail.fr>
2022-09-23 15:33:38 +0200

    add paragraph state

--- 08-lorem-ipsum/src/App.tsx
+++ 08-lorem-ipsum/src/App.tsx
@@ -2,17 +2,25 @@ import React, { useState } from "react";
 import data from "./data/data";
 
 function App() {
+  const [numberOfParagraphs, setNumebrOfParagraphs] = useState<number>(2);
+
+  const paragraphs = data.slice(0, numberOfParagraphs);
+
   return (
     <section className="section-center">
       <h3>TIRED OF BORING LOREM IPSUM?</h3>
       <form className="lorem-form">
         <label htmlFor="numberOfParagraphs">paragraphs: </label>
-        <input type="text" />
+        <input type="number" value={numberOfParagraphs} />
         <button type="submit" className="btn">
           generate
         </button>
       </form>
-      <p className="result"></p>
+      <article className="result">
+        {paragraphs.map((paragraph) => {
+          return <p>{paragraph}</p>;
+        })}
+      </article>
     </section>
   );
 }
