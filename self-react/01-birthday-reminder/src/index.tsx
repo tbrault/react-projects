e125a1ae3a0d58aa5fa6322f994173769a4dd902
tbrault <heymarin@hotmail.fr>
2022-12-03 14:36:32 +0100

    update repo structure

--- self-react/01-birthday-reminder/src/index.tsx
+++ self-react/01-birthday-reminder/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./css/index.css";
-import App from "../../01-birthday-reminder/src/App";
+import App from "./App";
 
 // ReactDOM.render(
 //   <React.StrictMode>
