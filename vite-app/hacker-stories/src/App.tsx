73ea41f202f54d888bf0ffcb03818c9bdc5cbf8f
tbrault <heymarin@hotmail.fr>
2023-01-08 18:39:14 +0100

    display searchedWebsites

--- vite-app/hacker-stories/src/App.tsx
+++ vite-app/hacker-stories/src/App.tsx
@@ -7,16 +7,20 @@ import websites from "./data/data";
 function App(): JSX.Element {
   const [searchTerm, setSearchTerm] = useState("");
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const websitesFiltered = websites.filter((website) =>
+    website.websiteCharacteristic.title.toLowerCase().includes(searchTerm)
+  );
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
     setSearchTerm(e.target.value);
-  };
+  }
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <Search search={searchTerm} onSearch={handleChange} />
       <hr />
-      <List websites={websites} />
+      <List websites={websitesFiltered} />
     </div>
   );
 }
