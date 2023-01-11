7a0ebcfb88d8f6e7985191e8985129550bcc0f05
tbrault <heymarin@hotmail.fr>
2023-01-11 11:51:47 +0100

    add about page

--- vite-app/cocktails/src/pages/About.tsx
+++ vite-app/cocktails/src/pages/About.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function About(): JSX.Element {
-  return <div>About</div>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga
+        temporibus dicta repellendus unde reprehenderit quam modi totam eos!
+        Iure doloremque eveniet quam mollitia! Ducimus, impedit cum provident
+        itaque animi corrupti!
+      </p>
+    </section>
+  );
 }
 
 export default About;
