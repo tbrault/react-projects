625a53b06fd6e4001058517cd1e669b6bf871994
tbrault <heymarin@hotmail.fr>
2023-01-11 10:58:30 +0100

    add navbar

--- vite-app/cocktails/src/App.tsx
+++ vite-app/cocktails/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App(): JSX.Element {
   return (
     <>
-      <div>App</div>
+      <Navbar />
       <Outlet />
     </>
   );
--- vite-app/cocktails/src/components/Navbar.tsx
+++ vite-app/cocktails/src/components/Navbar.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import logo from "../assets/logo.svg";
 
 function Navbar(): JSX.Element {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <img className="logo" src={logo} alt={logo} />
+        <div className="nav-links">
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
