6417d70b9ad5e61f60747a2fec79352dd260c739
tbrault <heymarin@hotmail.fr>
2023-01-12 15:58:26 +0100

    display singleCocktail

--- vite-app/cocktails/src/features/SingleCocktail/SingleCocktail.tsx
+++ vite-app/cocktails/src/features/SingleCocktail/SingleCocktail.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Button from "../../components/Button/Button";
 import Cocktail from "../../interface/cocktail";
+import styles from "./SingleCocktail.module.css";
 
 function SingleCocktail({
   id,
@@ -8,7 +10,21 @@ function SingleCocktail({
   info,
   glass,
 }: Cocktail): JSX.Element {
-  return <div>Cocktail</div>;
+  return (
+    <article className={styles.cocktail}>
+      <img src={image} alt={name} />
+      <footer className={styles.cocktailFooter}>
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Button
+          name="details"
+          path={`/cocktails/:${id}`}
+          buttonStyles={["btn", "btnPrimary"]}
+        />
+      </footer>
+    </article>
+  );
 }
 
 export default SingleCocktail;
