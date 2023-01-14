d594fbad4ad71f981f6a04ba2cac0e67002c302e
tbrault <heymarin@hotmail.fr>
2023-01-14 16:31:10 +0100

    add loading component

--- vite-app/cocktails/src/features/Loading/Loading.tsx
+++ vite-app/cocktails/src/features/Loading/Loading.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import styles from "./Loading.module.css";
 
 function Loading(): JSX.Element {
-  return <div>Loading</div>;
+  console.log("loading");
+
+  return <div className={styles.loader}></div>;
 }
 
 export default Loading;
--- vite-app/cocktails/src/pages/Home.tsx
+++ vite-app/cocktails/src/pages/Home.tsx
@@ -1,14 +1,16 @@
-import { Suspense } from "react";
-import Cocktails from "../features/Cocktails/Cocktails";
+import { lazy, Suspense } from "react";
+
 import Loading from "../features/Loading/Loading";
 import SearchBar from "../features/SearchBar/SearchBar";
+
+const LazyCocktails = lazy(() => import("../features/Cocktails/Cocktails"));
 
 function Home(): JSX.Element {
   return (
     <>
       <SearchBar />
       <Suspense fallback={<Loading />}>
-        <Cocktails />
+        <LazyCocktails />
       </Suspense>
     </>
   );
